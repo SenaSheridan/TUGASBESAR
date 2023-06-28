d6dbd151e21530963a355bb3aaf7b0ab1cc2379d
sena <senagardiana@gmail.com>
2023-06-28 22:43:35 +0700

    first commit

---
+++ application/views/v_footer.php
@@ -0,0 +1,35 @@
+<footer id="fh5co-footer" role="contentinfo">
+
+		<div class="container">
+			<div class="col-md-3 col-sm-12 col-sm-push-0 col-xs-12 col-xs-push-0">
+				<h3>TRISIGMA</h3>
+				<img width="250px" src="<?php echo base_url().'theme/images/trisigma_logo.png'?>" alt="" srcset="">
+			</div>
+			<div class="col-md-3 col-md-push-1 col-sm-12 col-sm-push-0 col-xs-12 col-xs-push-0">
+				<h3>Our Services</h3>
+				<ul class="float">
+					<li><a href="<?php echo base_url().'portfolio'?>">Web Design</a></li>
+				</ul>
+
+			</div>
+			<div class="col-md-3 col-sm-12 col-sm-push-0 col-xs-12 col-xs-push-0">
+				<h3>Information Contact</h3>
+				<ul class="float">
+						<li><i class="icon-map"></i> Alamat</li>
+						<li><i class="icon-phone"></i> +62 No telp</li>
+						<li><i class="icon-envelope"></i><a href="#"> trisigma7@gmail.com</a></li>
+					</ul>
+			</div>
+			<div class="col-md-2 col-md-push-1 col-sm-12 col-sm-push-0 col-xs-12 col-xs-push-0">
+				<h3>Follow Us</h3>
+				<ul class="fh5co-social">
+					<li><a href="#"><i class="icon-twitter"></i></a></li>
+					<li><a href="#"><i class="icon-facebook"></i></a></li>
+					<li><a href="#"><i class="icon-google-plus"></i></a></li>
+					<li><a href="#"><i class="icon-instagram"></i></a></li>
+				</ul>
+			</div>
+
+
+		</div>
+	</footer>
